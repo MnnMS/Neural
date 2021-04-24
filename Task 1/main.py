2dbc335019700345facb5ea088dc074f8b038472
MohamedAdel769 <mohamed.adel90211@gmail.com>
2021-04-24 09:51:11 +0200

    Main steps

---
+++ Task 1/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('IrisData.txt')
+#print(dataset)
+
+# Todo 1.replace class names with 0,1,2
+
+# Todo 2.draw Iris data
+
+# Todo 3.GUI
+
+# Todo 4.Call perceptron
+
+# Todo 5.Test
