6f070d3dcb90deac7a1295cb4eba87e5ff64801e
Mostafakhaled999 <38161411+Mostafakhaled999@users.noreply.github.com>
2021-04-26 23:23:09 +0200

    Gui Finished

--- Task 1/main.py
+++ Task 1/main.py
@@ -1,9 +1,48 @@
 import pandas as pd
+from tkinter import *
+from tkinter import ttk
 import matplotlib.pyplot as plt
 import train
 
 dataset = pd.read_csv('IrisData.txt')
 print(dataset)
+def Train():
+    print(f1_combo.get())
+def test():
+    print("test")
+
+#tr = train()
+mainForm = Tk()
+mainForm.geometry("650x500")
+mainForm.title("Task 1")
+
+f1_label = Label(mainForm,text = "Select Feature 1").place(x = 5, y = 10)
+f2_label = Label(mainForm,text ="Select Feature 2").place(x = 5, y = 50)
+clss_label = Label(mainForm,text ="Select Classes").place(x = 5, y = 100)
+lrnRate_label = Label(mainForm,text ="Enter Learning Rate").place(x = 5, y = 150)
+epochs_label = Label(mainForm,text ="Enter number of epochs").place(x = 5, y = 200)
+
+
+features = ('X1','X2','X3','X4')
+f1_combo = ttk.Combobox(mainForm,width = 10, values = features)
+f1_combo.place(x = 100, y = 10)
+f2_combo = ttk.Combobox(mainForm, width = 10, values = features)
+f2_combo.place(x = 100, y = 50)
+classes = ['C1 and C2', 'C1 and C3', 'C2 and C3']
+
+clss_combo = ttk.Combobox(mainForm,width=10, values = classes)
+clss_combo.place(x = 100, y = 100)
+lrnRate_txt = Entry(mainForm).place(x = 150, y = 150)
+epochs_txt = Entry(mainForm).place(x = 150, y = 200)
+bias_check = Checkbutton(mainForm,text = "Bias").place(x = 5, y = 250)
+
+train_button = Button(mainForm,text = "Train",command = Train).place(x = 300, y = 300)
+test_button = Button(mainForm,text = "Test",command = test).place(x = 400, y = 300)
+
+
+
+
+mainForm.mainloop()
 
 # Todo 1.replace class names with 0,1,2
 
